e74d1922c77898e03ef1ee6debe261adbde6e12e
Chanthavongphet <pmctah@gmail.com>
2025-03-04 18:54:27 +0700

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    domains: ['https://portfolio-tah-15.netlify.app/'],
+  },
 };
 
 export default nextConfig;
